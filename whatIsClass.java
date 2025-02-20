155004fea980db40788ca7eba13b40786b44393a
Madhur6 <madhur.20eic525@rtu.ac.in>
2025-02-20 10:55:14 +0530

    Added what is class

---
+++ whatIsClass.java
@@ -0,0 +1,111 @@
+// .java file ----------------> 
+
+// class Person {
+//     String name; // Declaring name here only once...
+//     public Person(String name){
+//         this.name = name;
+//     }
+// }
+
+// // Teacher is inheriting a property called "name" from Person...
+// // Studentt is inheriting a property called "name" from Person...
+// class Teacher extends Person {
+//     public Teacher(String name){
+//         super(name);
+//     }
+// }
+
+// class Studentt extends Person{
+//     public Studentt (String name){
+//         super(name);
+//     }
+// }
+
+
+
+
+public class whatIsClass {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+        add(1,2);
+    }
+
+    public static int add(int a, int b){
+        int x;
+        x = a + b; 
+        return x;
+    }
+}
+
+
+
+// Class 
+
+// Blueprint: There is a building > it should have it's construction details...
+
+// Object Oriented Style of Programming:
+class Person {
+    // 
+}
+
+// Maria
+
+// class: Class holds properties of whatever the name following class is...
+// Class: Class is a blueprint that defines structure of an real world entity...
+
+// Person,
+
+// Object: Instance of a class...
+
+// Object: Falls under a specific category / class...
+
+
+//                    Java Virtual Machine (s/w)
+// -------------------------> JVM ----------------------> Objects
+//  with the help of class     |          creates
+                    //         v
+                    //     properties / states
+
+class Studentt {
+    // Tania: An object of class Studentt
+    // Maria: An ----
+    // else: 
+}
+
+class Human {
+    // Madhur,
+    // Maria, : An object of class Human
+    // Harry : An object of class Human
+}
+
+class Plant {
+    // Mango-Tree, : An object of class Plant 
+}
+
+class Animal {
+    // Dog, Cat
+}
+
+class Insect {
+
+}
+
+
+class Vehicle {
+    // Honda Civic
+}
+
+
+// OOPs
+
+// Style of programming where we organize our code around data instead of logic  & objects rather than action
+// Pillars of OOPs
+
+// Inheritence : Child is inheriting properties from parents. [Person [Parent] Child[Teacher, Student] ]
+
+// Polymorphism : 
+
+// Abstraction : 
+
+// Encapsulation
